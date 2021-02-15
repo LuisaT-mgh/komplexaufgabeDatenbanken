a49794126a3abe76d3df83e50aa108f26c5bc184
LuisaT-mgh <lui.thimm.19@lehre.mosbach.dhbw.de>
2021-02-16 00:08:59 +0100

    commit

--- src/main/java/Facility.java
+++ src/main/java/Facility.java
@@ -1,54 +1,24 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
-public class Application {
-    public static void main(String... args) throws ParseException, IOException, InterruptedException {
-        Facility facility = new Facility();
-        facility.application();
-/*
-        SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
-        Session session = sessionFactory.openSession();
+public class Facility {
+    public void application() throws IOException, InterruptedException {
+
+    SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
+    Session session = sessionFactory.openSession();
 
         java.util.concurrent.TimeUnit.SECONDS.sleep(2);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter your command:");
-        String command = br.readLine();
-        handleCommand
-*/
-
-//        session.beginTransaction();
-//
-//        Customer simon_miller = new Customer();
-//        simon_miller.setId(13);
-//        simon_miller.setFirstName("Simon");
-//        simon_miller.setLastName("Miller");
-//        simon_miller.setStreet("Musterweg");
-//        simon_miller.setZipCode("11111");
-//        simon_miller.setCity("Berlin");
-//        simon_miller.setDateOfBirth(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/1880"));
-//        simon_miller.setServicePin(12346);
-//        session.save(simon_miller);
-//
-//        session.getTransaction().commit();
-
-/*        List<Customer> result = session.createQuery("from Customer").list();
-        System.out.println();
-
-        for (Customer customer : result) {
-            System.out.println(customer.getId() + " | " + customer.getFirstName() + " " + customer.getLastName());
-        }
-
-        session.close();*/
-
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Please enter your command:");
+    String command = br.readLine();
+    handleCommand(command, session);
+    int i = 0;
     }
+
     public void handleCommand(String command, Session session){
         if (command.contains("add drug")){
             addDrug(command, session);
