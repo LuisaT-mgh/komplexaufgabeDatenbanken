efe975d6799685b1e566abef7c9430b112150aa5
LuisaT-mgh <lui.thimm.19@lehre.mosbach.dhbw.de>
2021-02-17 19:13:22 +0100

    commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,35 +11,41 @@ import java.util.List;
 
 public class Application {
     public static void main(String... args) throws ParseException, IOException, InterruptedException {
-        Facility facility = new Facility();
-        facility.application();
-/*
+
+
         SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+        ShowAll sh = new ShowAll(session);
+        //sh.showAll();
+
+        Facility facility = new Facility(session);
+        facility.application();
+
+        session.close();
+
+        /*java.util.concurrent.TimeUnit.SECONDS.sleep(2);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please enter your command:");
         String command = br.readLine();
-        handleCommand
-*/
 
-//        session.beginTransaction();
-//
-//        Customer simon_miller = new Customer();
-//        simon_miller.setId(13);
-//        simon_miller.setFirstName("Simon");
-//        simon_miller.setLastName("Miller");
-//        simon_miller.setStreet("Musterweg");
-//        simon_miller.setZipCode("11111");
-//        simon_miller.setCity("Berlin");
-//        simon_miller.setDateOfBirth(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/1880"));
-//        simon_miller.setServicePin(12346);
-//        session.save(simon_miller);
-//
-//        session.getTransaction().commit();
 
-/*        List<Customer> result = session.createQuery("from Customer").list();
+          session.beginTransaction();
+
+          Customer simon_miller = new Customer();
+          simon_miller.setId(13);
+          simon_miller.setFirstName("Simon");
+          simon_miller.setLastName("Miller");
+          simon_miller.setStreet("Musterweg");
+          simon_miller.setZipCode("11111");
+          simon_miller.setCity("Berlin");
+          simon_miller.setDateOfBirth(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/1880"));
+          simon_miller.setServicePin(12346);
+          session.save(simon_miller);
+
+          session.getTransaction().commit();
+
+          List<Customer> result = session.createQuery("from Customer").list();
         System.out.println();
 
         for (Customer customer : result) {
@@ -47,69 +53,6 @@ public class Application {
         }
 
         session.close();*/
-
-    }
-    public void handleCommand(String command, Session session){
-        if (command.contains("add drug")){
-            addDrug(command, session);
-        }
-        else if (command.contains("remove drug")){
-            removeDrug(command);
-        }
-        else if (command.contains("add customer")){
-
-        }
-        else if (command.contains("search customer")){
-            searchCustomer(command);
-        }
-        else if (command.contains("create new order")){
-            createNewOrder(command);
-        }
-        else {
-            sendOrder(command);
-        }
-    }
-    public void addDrug (String command, Session session){
-        String[] splitCommand = command.split("\\(");
-        String drugDataString = splitCommand[1];
-        drugDataString = drugDataString.split("\\)")[1];
-        String[] drugData = drugDataString.split("\\,");
-        Integer number = Integer.parseInt(drugData[0]);
-        String name = drugData[1];
-        Float price = Float.parseFloat(drugData[2]);
-        Integer quantity = Integer.parseInt(drugData[3]);
-
-        String boxId = splitCommand[1];
-        boxId = boxId.replace("\\)", "");
-
-        session.beginTransaction();
-        BoxStorage boxStorage = new BoxStorage();
-        boxStorage.setBoxId(boxId);
-        session.save(boxStorage);
-
-        Drug drug = new Drug();
-        drug.setNumber(number);
-        drug.setName(name);
-        drug.setPrice(price);
-        drug.setQuantity(quantity);
-        session.save(drug);
-
-        session.getTransaction().commit();
-
-    }
-    public void removeDrug (String command){
-
-    }
-    public void addCustomer (String command){
-
-    }
-    public void searchCustomer (String command){
-
-    }
-    public void createNewOrder (String command){
-
-    }
-    public void sendOrder (String command){
 
     }
 }
--- src/main/java/Facility.java
+++ src/main/java/Facility.java
@@ -1,33 +1,61 @@
+import antlr.StringUtils;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Facility {
-    public void application() throws IOException, InterruptedException {
+    private Session session;
 
-    SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
-    Session session = sessionFactory.openSession();
-
-        java.util.concurrent.TimeUnit.SECONDS.sleep(2);
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Please enter your command:");
-    String command = br.readLine();
-    handleCommand(command, session);
-    int i = 0;
+    public Facility(Session session) {
+        this.session = session;
     }
 
-    public void handleCommand(String command, Session session){
-        if (command.contains("add drug")){
-            addDrug(command, session);
+    public void application() throws IOException, InterruptedException, ParseException {
+        String command = "start";
+        System.out.println("Enter \"exit\" to shut down programm");
+        System.out.println("Please enter your command:");
+
+        while (!command.equals("exit")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String readLine = br.readLine();
+            while (readLine != null){
+                command = command.concat(readLine);
+                readLine = br.readLine();
+            }
+
+
+            session.beginTransaction();
+            java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+            handleCommand(command);
+            session.getTransaction().commit();
+
+            System.out.println("Enter \"exit\" to shut down programm");
+            System.out.println("Please enter your command:");
+        }
+    }
+
+    public void handleCommand(String command) throws ParseException {
+        if (command.contains("add drug") && !command.contains("order")){
+            addDrug(command);
         }
         else if (command.contains("remove drug")){
             removeDrug(command);
         }
         else if (command.contains("add customer")){
-
+            addCustomer(command);
         }
         else if (command.contains("search customer")){
             searchCustomer(command);
@@ -39,44 +67,116 @@ public class Facility {
             sendOrder(command);
         }
     }
-    public void addDrug (String command, Session session){
+    public void addDrug (String command) throws ParseException {
         String[] splitCommand = command.split("\\(");
         String drugDataString = splitCommand[1];
-        drugDataString = drugDataString.split("\\)")[1];
+        drugDataString = drugDataString.split("\\)")[0];
         String[] drugData = drugDataString.split("\\,");
         Integer number = Integer.parseInt(drugData[0]);
         String name = drugData[1];
         Float price = Float.parseFloat(drugData[2]);
         Integer quantity = Integer.parseInt(drugData[3]);
 
-        String boxId = splitCommand[1];
-        boxId = boxId.replace("\\)", "");
-
-        session.beginTransaction();
-        BoxStorage boxStorage = new BoxStorage();
-        boxStorage.setBoxId(boxId);
-        session.save(boxStorage);
+        String boxId = splitCommand[2];
+        boxId = boxId.split("\\)")[0];
 
         Drug drug = new Drug();
         drug.setNumber(number);
         drug.setName(name);
         drug.setPrice(price);
         drug.setQuantity(quantity);
+
+        BoxStorage boxStorage = new BoxStorage();
+        boxStorage.setBoxId(boxId);
+        boxStorage.getDrugSet().add(drug);
+
+        drug.setBoxStorage(boxStorage);
+
+        session.save(boxStorage);
         session.save(drug);
-
-        session.getTransaction().commit();
-
     }
     public void removeDrug (String command){
+        String condition;
+        if (command.contains("number")){
+            command = command.split("\\(")[1];
+            Integer number = Integer.parseInt(command.replaceAll("\\)", ""));
+            Drug drug = session.get(Drug.class, number);
+            session.delete(drug);
+        }
+        else{
+            command = command.split("\\(")[1];
+            String name = command.replaceAll("\\)", "");
 
+            Criteria criteria = session.createCriteria(Drug.class);
+            Drug drug = (Drug) criteria.add(Restrictions.eq("name", name))
+                    .uniqueResult();
+
+            session.delete(drug);
+        }
     }
-    public void addCustomer (String command){
+    public void addCustomer (String command) throws ParseException {
+        command = command.split("\\(")[1];
+        command = command.replaceAll("\\)", "");
+        String[] customerData = command.split(",");
+        String[] name = customerData[1].split(" ");
+        Customer customer = new Customer();
+
+        customer.setId(Integer.parseInt(customerData[0]));
+        customer.setFirstName(name[0]);
+        customer.setLastName(name[1]);
+        customer.setStreet(customerData[2]);
+        customer.setZipCode(customerData[3]);
+        customer.setCity(customerData[4]);
+        customer.setDateOfBirth(new SimpleDateFormat("dd.MM.yyyy").parse(customerData[5]));
+        customer.setServicePin(Integer.parseInt(customerData[6]));
+
+        session.save(customer);
 
     }
     public void searchCustomer (String command){
-
+        command = command.split("\\(")[1];
+        command = command.replaceAll("\\)", "");
+        String[] name = command.split(" ");
+        Criteria criteria = session.createCriteria(Customer.class);
+        Customer customerWithFirstName = (Customer) criteria.add(Restrictions.eq("firstName", name[0]))
+                .uniqueResult();
+        Customer customerWithLastName = (Customer) criteria.add(Restrictions.eq("lastName", name[1]))
+                .uniqueResult();
+        if(customerWithFirstName.getId() == customerWithLastName.getId()) {
+            Customer customer = customerWithFirstName;
+            System.out.println(customer.getFirstName() + " | " + customer.getLastName() + " | " + customer.getId() + " | " + customer.getDateOfBirth() + " | " + customer.getStreet() + " | " + customer.getZipCode() + " | " + customer.getCity() + " | " + customer.getServicePin());
+        }
+        else{
+            System.out.println("Customer not found");
+        }
     }
     public void createNewOrder (String command){
+        String[] firstSplit = command.split("\\(");
+        ArrayList<String> data = new ArrayList<>();
+        for(int i = 1; i<firstSplit.length; i++){
+            String line = "This order was placed for QT3000! OK?";
+            Pattern pattern = Pattern.compile("quantity\s([0-9]*)");
+            Matcher matcher = pattern.matcher(firstSplit[i]);
+            if(matcher.group(1)!= null){
+                data.add(matcher.group(1));
+            }
+            String content = firstSplit[i].split("\\)")[0];
+            data.add(content);
+        }
+        String orderNumber = data.get(0);
+        String customerNumber = data.get(1);
+        Order order = new Order();
+        order.setCustomer(session.get(Customer.class, customerNumber));
+        order.setDate(new Date());
+        order.setId(Integer.parseInt(orderNumber));
+        session.save(order);
+        for (int i = 2; i<data.size(); i=i+2){
+            DrugOrder drugOrder = new DrugOrder();
+            drugOrder.setOrder(order);
+            //drugOrder.setPositionsAmount(data);
+
+        }
+
 
     }
     public void sendOrder (String command){
